1cca3e1278ad7239c0ccea9b05850ccbc8d2d01f
logan lugez <binglei.pro@gmail.com>
2025-05-22 16:25:19 +0200

    start booking feature

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your models here.
+
+class Article:
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
