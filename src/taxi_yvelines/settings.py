b114fd6e173e2f93948e3679afed762d354688e3
Logan <loganlugez8@gmail.com>
2025-11-22 11:45:19 +0100

    bump css version

--- src/taxi_yvelines/settings.py
+++ src/taxi_yvelines/settings.py
@@ -139,7 +139,7 @@ else:
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 
 # Default primary key field type
